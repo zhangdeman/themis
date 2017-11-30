6af0fd345ae22ccec225b12276fa550f82efa82e
张德满 <2215508028@qq.com>
2017-11-30 23:43:45 +0800

    update

--- Api/Out.php
+++ Api/Out.php
@@ -12,10 +12,16 @@ class Out
     const ERROR_GET_ID_FAIL = 102; //获取ID信息失败
 
     const ERROR_DIFF_PASSWORD = 201; //两次密码不一致
+    const ERROR_NIL_ACCOUNT =   202;    //用户不存在
+    const ERROR_ADMIN_ACCOUNT_EXCEPTION = 203; //账户异常
+    const ERROR_ADMIN_ACCOUNT_NO_MATCH = 204; //账号或密码异常
 
     public static $ERROR_MSG_MAP = array(
         self::ERROR_PARAMS_ERROR => '参数错误',
         self::ERROR_DIFF_PASSWORD => '两次密码不一致',
+        self::ERROR_NIL_ACCOUNT =>  '用户不存在',
+        self::ERROR_ADMIN_ACCOUNT_EXCEPTION =>  '账号异常',
+        self::ERROR_ADMIN_ACCOUNT_NO_MATCH  =>  '账号或密码错误',
     );
 
     /**
