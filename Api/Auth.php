abd1331af952276c922671aaa8c2251163094c58
张德满 <2215508028@qq.com>
2018-01-28 14:00:17 +0800

    update

---
+++ Api/Auth.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhangdeman
+ * Date: 2018/1/28
+ * Time: 13:43
+ */
+namespace Themis\Api;
+
+class Auth
+{
+    //管理后台的app_id
+    const APP_ID_ADMIN = 101;
+    const APP_NAME_ADMIN = 'admin-server';
+
+    //用户端（web）app_id
+    const APP_ID_APP_SERVER = 102;
+    const APP_NAME_APP_SERVER = 'app-server';
+}
